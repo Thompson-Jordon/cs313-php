2fa19193c09eeca6f9aca9cca73b9aae3e169606
Jordon Thompson <tho17088@byui.edu>
2020-01-15 21:44:20 -0700

    test nav css

--- web/nav.php
+++ web/nav.php
@@ -1,4 +1,4 @@
-<nav id="navigation" class="navbar navbar-expand-sm fixed-top bg-primary navbar-dark">
+<nav id="navigation" class="navbar navbar-expand-sm bg-primary navbar-light">
    <ul class="navbar-nav">
       <li class="nav-item" <?php if ($thisPage == "Home") echo " id=\"currentpage\""; ?>><a class="nav-link" href="index.php">Home</a></li>
       <li class="nav-item" <?php if ($thisPage == "Teach Assignments") echo " id=\"currentpage\""; ?>><a class="nav-link" href="teachAssign.php">Teach Assignments</a></li>
