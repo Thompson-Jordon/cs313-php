fe4f977be9319ea9bea6e43a3fde5e272788203d
Jordon Thompson <tho17088@byui.edu>
2020-01-15 21:55:36 -0700

    add active bootstrap to menu item

--- web/nav.php
+++ web/nav.php
@@ -1,7 +1,7 @@
 <nav id="navigation" class="navbar navbar-expand-sm bg-primary navbar-dark">
    <ul class="navbar-nav">
-      <li class="nav-item" <?php if ($thisPage == "Home") echo " id=\"currentpage\""; ?>><a class="nav-link" href="index.php">Home</a></li>
-      <li class="nav-item" <?php if ($thisPage == "Teach Assignments") echo " id=\"currentpage\""; ?>><a class="nav-link" href="teachAssign.php">Teach Assignments</a></li>
-      <li class="nav-item" <?php if ($thisPage == "Prove Assignments") echo " id=\"currentpage\""; ?>><a class="nav-link" href="proveAssign.php">Prove Assignments</a></li>
+      <li class="nav-item <?php if ($thisPage == "Home") echo " active"; ?>"><a class="nav-link" href="index.php">Home</a></li>
+      <li class="nav-item <?php if ($thisPage == "Prove Assignments") echo " active"; ?>"><a class="nav-link" href="proveAssign.php">Prove Assignments</a></li>
+      <li class="nav-item <?php if ($thisPage == "Teach Assignments") echo " active"; ?>"><a class="nav-link" href="teachAssign.php">Teach Assignments</a></li>
    </ul>
 </nav>
