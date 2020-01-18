755cb6ff172e76dd1ebe66e8a69764b729b123d8
Jordon Thompson <tho17088@byui.edu>
2020-01-18 15:44:45 -0700

    edit nav

--- web/nav.php
+++ web/nav.php
@@ -1,7 +1,7 @@
-<nav id="navigation" class="navbar sticky-top navbar-expand-sm bg-primary navbar-dark">
+<nav id="navigation" class="navbar sticky-top navbar-expand-sm bg-primary navbar-dark">CS 470
    <ul class="navbar-nav">
       <li class="nav-item <?php if ($thisPage == "Home") echo " active"; ?>"><a class="nav-link" href="index.php">Home</a></li>
-      <li class="nav-item <?php if ($thisPage == "Prove Assignments") echo " active"; ?>"><a class="nav-link" href="proveAssign.php">Prove Assignments</a></li>
       <li class="nav-item <?php if ($thisPage == "Teach Assignments") echo " active"; ?>"><a class="nav-link" href="teachAssign.php">Teach Assignments</a></li>
-   </ul>
+      <li class="nav-item <?php if ($thisPage == "Prove Assignments") echo " active"; ?>"><a class="nav-link" href="proveAssign.php">Prove Assignments</a></li>
+   </ul>Jordon Thompson
 </nav>
