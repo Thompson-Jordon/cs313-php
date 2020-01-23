2d7cf7318de455e0dd0d201c15d3dbe95903b4f7
Jordon Thompson <tho17088@byui.edu>
2020-01-22 21:13:13 -0700

    found austrailia ln6

--- web/teach_03/form_03.php
+++ web/teach_03/form_03.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $comments = $_POST['comments'];
 $major = $_POST['major'];
 $input_continents = $_POST['continents'];
-$continents = ['NA' => 'North America', 'SA' => 'South America', 'EU' => 'Europe', 'AS' => 'Asia', 'AF' => 'Africa', 'AN' => 'Antarctica'];
+$continents = ['NA' => 'North America', 'SA' => 'South America', 'EU' => 'Europe', 'AS' => 'Asia', 'AU' => 'Australia', 'AF' => 'Africa', 'AN' => 'Antarctica'];
 
 echo '<p>Your name is ' . $name . '</p>';
 echo '<p>Your email is <a href="mailto:' . $email . '">' . $email . '</a></p>';
